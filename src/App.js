3c22c0964ed0c6912547a2b34c3080bed542e6d0
Armova <arnoldomora79@gmail.com>
2021-11-24 16:59:34 -0600

    cambio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import { useState } from 'react';
 import './App.css';
-import ContadorIncremental from './contadorIncremental';
+import ContadorIncremental from './ContadorIncremental';
 
 function App() {
   const [contador, setContador] = useState(0);
@@ -13,14 +13,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload!
           <p>
-            Este va a ser el sitio web de Minor Vargas Selles!
+            Este va a ser el sitio web de Minor Vargas Selles!!!
           </p>
-          <ContadorIncremental incremento={10} valorInicial={99} velocidad={3000}>
-          </ContadorIncremental>
-          <ContadorIncremental incremento={-145} valorInicial={20} velocidad={500}>
-          </ContadorIncremental>
+          <ContadorIncremental incremento={10} valorInicial={99} velocidad={3000} />
+          <ContadorIncremental incremento={-145} valorInicial={20} velocidad={500} />
         </div>
         <a
           className="App-link"
